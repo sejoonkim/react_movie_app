47c52208e1d5e8356791b2c83b45a5e7b27d99ab
sejoonkim <sejoondev@gmail.com>
2020-04-22 12:13:49 +0900

    Updates

---
+++ precache-manifest.4b284a2920bd20ccab0cdae7e40aa56f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c92552e5670a0f3e9bca1477f73ebb2",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "1101acb3415d0ee9413e",
+    "url": "/react_movie_app/static/css/main.3e69f5fe.chunk.css"
+  },
+  {
+    "revision": "a53c9ff4d1ef323dfa5f",
+    "url": "/react_movie_app/static/js/2.cf423e27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.cf423e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1101acb3415d0ee9413e",
+    "url": "/react_movie_app/static/js/main.d7c59d6c.chunk.js"
+  },
+  {
+    "revision": "af7792b39b908266633c",
+    "url": "/react_movie_app/static/js/runtime-main.a431237d.js"
+  }
+]);
